71a688506679c8f804b4be4a7d2f400f88d21c63
IrfanUllahKhanMohmand <irfankhanmhmnd@gmail.com>
2021-11-09 11:15:46 -0800

    first commit

---
+++ airline/airline/urls.py
@@ -0,0 +1,11 @@
+
+from django.conf.urls import include
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("flights/", include("flights.urls")),
+
+
+]
